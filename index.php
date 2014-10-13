e58c2e02e3792d3ecf3ed5abc9d78bf1c6237719
Ryan Maskell <Ryan@SavorWeb.com>
2014-10-13 11:18:54 -0700

    added video

--- index.php
+++ index.php
@@ -149,6 +149,10 @@
 </div>
 
 
+<div class="row">
+	<style>.embed-container { position: relative; padding-bottom: 56.25%; margin-bottom: 40px; height: 0; overflow: hidden; max-width: 100%; height: auto; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='http://www.youtube.com/embed/E8368mC_q4A?rel=0&amp;controls=0&amp;showinfo=0' frameborder='0' allowfullscreen></iframe></div>
+</div>
+
 	<div class="row">
 		<div class="typeset">
 			<h1 id="portfolio-header" class="editable">See for yourself</h1>
